9cc7a0cbd98099b31eac249e91b1320611c38b9b
David Mackenzie <48002743+DavidAustinMackenzie@users.noreply.github.com>
2021-10-07 15:26:16 +1100

    added in sharedPreferences

--- app/src/main/java/au/edu/swin/sdmd/w09_calcuations_storage/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w09_calcuations_storage/MainActivity.kt
@@ -1,5 +1,6 @@
 package au.edu.swin.sdmd.w09_calcuations_storage
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -8,13 +9,20 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    //Class variables in order to access inputs outside onCreate()
+    lateinit var number1: EditText
+    lateinit var number2: EditText
+
+    override fun onCreate(savedInstancedState: Bundle?){
+        super.onCreate(savedInstancedState)
         setContentView(R.layout.activity_main)
 
+        //Check if sharePrefs are available
+        checkSharedPrefs()
+
         //Get input elements
-        val number1 = findViewById<EditText>(R.id.number1)
-        val number2 = findViewById<EditText>(R.id.number2)
+        number1 = findViewById<EditText>(R.id.number1)
+        number2 = findViewById<EditText>(R.id.number2)
         val equals = findViewById<Button>(R.id.equals)
         val radioAdd = findViewById<RadioButton>(R.id.radioAdd)
 
@@ -37,6 +45,33 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Used to save current values in editText fields
+    override fun onPause() {
+        super.onPause()
+
+        val pref = this.getSharedPreferences("file1",Context.MODE_PRIVATE) ?: return
+
+        with (pref.edit()){
+            val number1 = findViewById<EditText>(R.id.number1)
+            val number2 = findViewById<EditText>(R.id.number2)
+
+            putString("num1", number1.text.toString())
+            putString("num2", number2.text.toString())
+            apply()
+
+        }
+    }
+
+    //Get values from sharedPreferences
+    private fun checkSharedPrefs(){
+        val sharedPref = this.getSharedPreferences(getString(R.string.saved_numbers),
+        Context.MODE_PRIVATE)
+        sharedPref?.let{
+            number1.setText(sharedPref.getString(getString(R.string.number1),""))
+            number2.setText(sharedPref.getString(getString(R.string.number2),""))
+        }
+
+    }
     // adds two numbers together
     private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
 
